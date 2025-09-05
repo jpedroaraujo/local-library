2b3c2eb5022aaaf5fd6508991ccea20691d82a7c
Joao Pedro <joao.pedro@iqnus.com.br>
2025-09-05 17:01:51 -0300

    Add Mongoose connection setup and update dependencies in package.json

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -37,5 +39,27 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+
+const uri =
+  "mongodb+srv://joaoaoliveira202_db_user:<db_password>@cluster0.s2tram0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
 
 module.exports = app;
